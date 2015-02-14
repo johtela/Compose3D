206025ea72f948cfb8f713bc0e6690debefb8db3
Tommi Johtela <tommi.johtela@dnainternet.net>
2015-02-14 22:57:00 +0200

    Implemented more matrix functions.

---
+++ Compose3D/GLSL/Matf.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Compose3D.GLSL
+{
+    public static class Matf
+    {
+        public static M Identity<M> () where M : Mat<float>, new ()
+        {
+            var res = new M ();
+            for (int i = 0; i < Math.Min (res.Columns, res.Rows); i++)
+                res[i, i] = 1f;
+            return res;
+        }
+
+        public static M Translation<M> (params float[] offsets) where M : Mat<float>, new ()
+        {
+            var res = Identity<M> ();
+            var lastrow = res.Rows - 1;
+            for (int i = 0; i < offsets.Length; i++)
+                res[i, lastrow] = offsets[i];
+            return res;
+        }
+
+        public static M Scaling<M> (params float[] factors) where M : Mat<float>, new ()
+        {
+            var res = Identity<M> ();
+            for (int i = 0; i < factors.Length; i++)
+                res[i, i] = factors[i];
+            return res;
+        }
+
+        public static M RotationX<M> (float alpha) where M : Mat<float>, new ()
+        {
+            var res = Identity<M> ();
+            var sina = (float)Math.Sin (alpha);
+            var cosa = (float)Math.Cos (alpha);
+            res[1, 1] = cosa;
+            res[1, 2] = sina;
+            res[2, 1] = -sina;
+            res[2, 2] = cosa;
+            return res;
+        }
+
+        public static M RotationY<M> (float alpha) where M : Mat<float>, new ()
+        {
+            var res = Identity<M> ();
+            var sina = (float)Math.Sin (alpha);
+            var cosa = (float)Math.Cos (alpha);
+            res[0, 0] = cosa;
+            res[0, 2] = -sina;
+            res[2, 0] = sina;
+            res[2, 2] = cosa;
+            return res;
+        }
+
+        public static M RotationZ<M> (float alpha) where M : Mat<float>, new ()
+        {
+            var res = Identity<M> ();
+            var sina = (float)Math.Sin (alpha);
+            var cosa = (float)Math.Cos (alpha);
+            res[0, 0] = cosa;
+            res[0, 1] = sina;
+            res[1, 0] = -sina;
+            res[1, 1] = cosa;
+            return res;
+        }
+    }
+}
---
+++ Compose3D/GLSL/Vec.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Compose3D.GLSL
+{
+    public class Vec<T> : IEquatable<Vec<T>> where T : struct, IEquatable<T>
+    {
+        private T[] _vector;
+
+        protected Vec (T[] vector)
+        {
+            _vector = vector;
+        }
+
+        public T[] Vector
+        {
+            get { return _vector; }
+        }
+
+        public T this[int index]
+        {
+            get { return _vector[index]; }
+            set { _vector[index] = value; }
+        }
+
+        public bool Equals (Vec<T> other)
+        {
+            for (int i = 0; i < _vector.Length; i++)
+                if (!_vector[i].Equals(other._vector[i])) return false;
+            return true;
+        }
+
+        public override bool Equals (object obj)
+        {
+            var other = obj as Vec<T>;
+            return !ReferenceEquals (other, null) && _vector.Equals (other._vector);
+        }
+
+        public override int GetHashCode ()
+        {
+            return _vector.GetHashCode ();
+        }
+
+        public override string ToString ()
+        {
+            var sb = new StringBuilder ("[");
+            for (int i = 0; i < _vector.Length; i++)
+                sb.AppendFormat (" {0}", _vector[i].ToString ());
+            sb.Append (" ]");
+            return sb.ToString ();
+        }
+         
+        public static bool operator == (Vec<T> left, Vec<T> right)
+        {
+            return left.Equals (right);
+        }
+
+        public static bool operator != (Vec<T> left, Vec<T> right)
+        {
+            return !left.Equals (right);
+        }
+    }
+}

7567aeb2ab1391f1b5c2922f49d2de96015ed8f1
Tommi <tommi.johtela@dnainternet.net>
2015-09-10 16:51:07 +0300

    Started to write Octree data structure.

---
+++ Compose3D/Geometry/Octree.cs
@@ -0,0 +1,25 @@
+﻿namespace Compose3D.Geometry
+{
+    using System;
+    using Arithmetics;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Octree<T, U, V> 
+        where V : struct, IVec<V, T>
+        where T : struct, IEquatable<T>
+    {
+        private class Node
+        {
+            public Node[] Children;
+            public U Data;
+
+            public Node ()
+            {
+                Children = new Node[8];
+            }
+        }
+
+
+    }
+}

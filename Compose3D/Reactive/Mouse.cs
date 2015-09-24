56c89095c7cc0858873933bed1d43457b7c87e86
tommi <tommi.johtela@dnainternet.net>
2015-09-25 01:08:09 +0300

    Started writing FRP code.

---
+++ Compose3D/Reactive/Mouse.cs
@@ -0,0 +1,20 @@
+﻿namespace Compose3D.Reactive
+{
+	using System;
+	using OpenTK;
+	using OpenTK.Input;
+	using Arithmetics;
+
+	public static class Mouse
+	{
+		public static void MouseMove (this Reaction<MouseMoveEventArgs> reaction, GameWindow window)
+		{
+			window.MouseMove += (object sender, MouseMoveEventArgs e) => reaction (e);
+		}
+
+		public static void MouseXY (this Reaction<Vec2> reaction, GameWindow window)
+		{
+			reaction.Map<MouseMoveEventArgs, Vec2> (e => new Vec2 (e.X, e.Y)).MouseMove (window);
+		}
+	}
+}
---
+++ Compose3D/Reactive/Reaction.cs
@@ -0,0 +1,34 @@
+﻿namespace Compose3D.Reactive
+{
+	using System;
+
+	public delegate void Reaction<T> (T input);
+
+	public static class Reaction
+	{
+		public static Reaction<T> Lift<T> (Action<T> action)
+		{
+			return input => action (input);
+		}
+
+		public static Reaction<T> Map<T, U> (this Reaction<U> reaction, Func<T, U> func)
+		{
+			return input => reaction (func (input));
+		}
+
+		public static Reaction<T> Filter<T> (this Reaction<T> reaction, Func<T, bool> predicate)
+		{
+			return input =>
+			{
+				if (predicate (input))
+					reaction (input);
+			};
+		}
+
+//		public static Reaction<T> MergeWith<T, U> (this Reaction<T> reaction, Reaction<U> other, Func<T, U, T> func)
+//		{
+////			return input => 
+//		}
+	}
+}
+

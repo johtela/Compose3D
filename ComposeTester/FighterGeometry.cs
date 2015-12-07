e52fdcfcebb0ddc8d0724ae2b33668e27e3dc29d
Tommi Johtela <tommi.johtela@dnainternet.net>
2015-12-08 00:14:34 +0200

    Added parameters for adjusting the fighter geometry.

--- ComposeTester/FighterGeometry.cs
+++ ComposeTester/FighterGeometry.cs
@@ -22,16 +22,16 @@
 			public readonly Geometry<V> Cone;
 			public readonly Path<P, Vec3> XSection;
 
-			public Nose ()
+			public Nose (float middleHeight, float baseHeight, int numPoints, float bottomFlatness)
 			{
 				var spline = BSpline<Vec3>.FromControlPoints (2,
-					new Vec3 (-1f, -0f, 0f),
-					new Vec3 (0f, 0.5f, 0f),
-					new Vec3 (1f, 0.6f, 0f));
+					new Vec3 (-1f, 0f, 0f),
+					new Vec3 (0f, middleHeight, 0f),
+					new Vec3 (1f, baseHeight, 0f));
 				Profile = Path<P, Vec3>.FromBSpline (spline, 8);
 
-				Cone = Lathe<V>.Turn (Profile, Axis.X, new Vec3 (0f), MathHelper.Pi / 13f, 0f, 0f)
-					.ManipulateVertices (Manipulators.Scale<V> (1f, 0.6f, 1f).Where (v => v.Position.Y < 0f))
+				Cone = Lathe<V>.Turn (Profile, Axis.X, new Vec3 (0f), MathHelper.TwoPi / numPoints, 0f, 0f)
+					.ManipulateVertices (Manipulators.Scale<V> (1f, 1f - bottomFlatness, 1f).Where (v => v.Position.Y < 0f))
 					.RotateY (90f.Radians ());
 
 				XSection = Path<P, Vec3>.FromVecs (
@@ -46,10 +46,11 @@
 			public readonly Path<P, Vec3> XSection;
 			public readonly P XSectionStart;
 
-			public CockpitFuselage (Nose nose)
+			public CockpitFuselage (Nose nose, float length, float baseScale, float bend)
 			{
 				Fuselage = Solids.Extrude<V, P> (false, false, nose.XSection,
-				   nose.XSection.Transform (Mat.Translation<Mat4> (0f, 0, -1.2f) * Mat.Scaling<Mat4> (1f, 1.2f, 1.2f)));
+					nose.XSection.Transform (Mat.Translation<Mat4> (0f, 0, -length) * 
+						Mat.Scaling<Mat4> (1f, baseScale, baseScale)));
 
 				Fuselage = Composite.Create (Stacking.StackBackward (nose.Cone, Fuselage));
 				XSection = Path<P, Vec3>.FromVecs (
@@ -58,7 +59,7 @@
 					select v.Position).Close ();
 				var pivotPoint = XSection.Nodes.Furthest (Dir3D.Up).First ().Position;
 				Fuselage = Fuselage.ManipulateVertices (Manipulators.Transform<V> (
-					Mat.RotationX<Mat4> (5f.Radians ()).RelativeTo (pivotPoint))
+					Mat.RotationX<Mat4> (bend.Radians ()).RelativeTo (pivotPoint))
 					.Where (v => v.Position.Z > pivotPoint.Z));
 
 				XSectionStart = XSection.Nodes.Furthest (Dir3D.Down + Dir3D.Left).Single ();
@@ -133,7 +134,7 @@
 
 				BellyXSection = Path<P, Vec3>.FromVecs (
 					from v in cockpitFuselage.Fuselage.Vertices.Furthest (Dir3D.Back)
-					where v.Position.Y < -0.2f
+					where v.Position.Y < -0.1f
 					select v.Position);
 				Belly = Ext.Enumerate (BellyXSection, BellyXSection.Transform (
 						Mat.Translation<Mat4> (0f, 0f, -1f) * Mat.Scaling<Mat4> (1.45f, 1f, 1f)))
@@ -144,9 +145,9 @@
 			{
 				var cPoints = new Vec3[]
 				{
-				start,
-				new Vec3 (start.X * 1.1f, bottom * 0.6f, start.Z),
-				new Vec3 (start.X * 0.8f, bottom * 1.0f, start.Z),
+					start,
+					new Vec3 (start.X * 1.1f, bottom * 0.6f, start.Z),
+					new Vec3 (start.X * 0.8f, bottom * 1.0f, start.Z),
 				};
 				var spline = BSpline<Vec3>.FromControlPoints (2,
 					cPoints.Append (new Vec3 (0f, bottom * 1.2f, start.Z))
@@ -158,16 +159,16 @@
 
 		public FighterGeometry ()
 		{
-			var nose = new Nose ();
-			var cockpitFuselage = new CockpitFuselage (nose);
+			var nose = new Nose (0.5f, 0.6f, 26, 0.4f);
+			var cockpitFuselage = new CockpitFuselage (nose, 1.2f, 1.2f, 3f);
 			var mainFuselage = new MainFuselage (cockpitFuselage);
 			var intake = new EngineIntake (cockpitFuselage);
 
 			LineSegments = from p in new Path<P, Vec3>[] { intake.XSection }
 						   select new LineSegment<P, Vec3> (p);
 
-			Fighter = Composite.Create (Stacking.StackBackward (cockpitFuselage.Fuselage, mainFuselage.Fuselage).Concat (
-				Ext.Enumerate (intake.Intake, intake.Belly)))
+			Fighter = Composite.Create (Stacking.StackBackward (cockpitFuselage.Fuselage, mainFuselage.Fuselage)
+				.Concat (Ext.Enumerate (intake.Intake, intake.Belly)))
 				.Smoothen (0.85f)
 				.Color (VertexColor<Vec3>.Chrome)
 				.Center ();

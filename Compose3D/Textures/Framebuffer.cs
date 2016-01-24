c3ea045cd5b79eb2e41e143ad5a3e77433eae7b2
tommi <tommi.johtela@dnainternet.net>
2016-01-24 23:53:12 +0200

    Started with framebuffers.

---
+++ Compose3D/Textures/Framebuffer.cs
@@ -0,0 +1,20 @@
+﻿namespace Compose3D.Textures
+{
+	using System;
+	using OpenTK.Graphics.OpenGL;
+
+	public class Framebuffer
+	{
+		private Texture _texture;
+		internal int _glFramebuffer;
+		
+		public Framebuffer (Texture texture)
+		{
+			_texture = texture;
+			_glFramebuffer = GL.GenFramebuffer ();
+		}
+		
+		
+	}
+}
+

a4175c6206460d0cae8631ce0a09b9f3e09c30bc
Tommi <tommi.johtela@dnainternet.net>
2016-04-18 16:01:16 +0300

    One file left out.

---
+++ Compose3D/Reactive/RenderReactions.cs
@@ -0,0 +1,131 @@
+﻿namespace Compose3D.Reactive
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using GLTypes;
+	using Textures;
+	using OpenTK.Graphics.OpenGL;
+
+	public static class Render
+	{
+		private static int GetState (GetPName parameter)
+		{
+			int value;
+			GL.GetInteger (parameter, out value);
+			return value;
+		}
+
+		public static Reaction<T> Culling<T> (this Reaction<T> render, CullFaceMode mode = CullFaceMode.Back,
+			FrontFaceDirection frontFace = FrontFaceDirection.Cw)
+		{
+			return input =>
+			{
+				var oldCap = GL.IsEnabled (EnableCap.CullFace);
+				var oldMode = GetState (GetPName.CullFaceMode);
+				var oldFrontFace = GetState (GetPName.FrontFace);
+
+				GL.Enable (EnableCap.CullFace);
+				GL.CullFace (mode);
+				GL.FrontFace (frontFace);
+				var result = render (input);
+				GL.FrontFace ((FrontFaceDirection)oldFrontFace);
+				GL.CullFace ((CullFaceMode)oldMode);
+				if (!oldCap)
+					GL.Disable (EnableCap.CullFace);
+				return result;
+			};
+		}
+
+		public static Reaction<T> DepthTest<T> (this Reaction<T> render, 
+			DepthFunction depthFunction = DepthFunction.Less)
+		{
+			return input =>
+			{
+				var oldCap = GL.IsEnabled (EnableCap.DepthTest);
+				var oldFunction = GetState (GetPName.DepthFunc);
+				var oldMask = GetState (GetPName.DepthWritemask);
+
+				GL.Enable (EnableCap.DepthTest);
+				GL.DepthMask (true);
+				GL.DepthFunc (depthFunction);
+				var result = render (input);
+				GL.DepthMask (oldMask != 0);
+				GL.DepthFunc ((DepthFunction)oldFunction);
+				if (!oldCap)
+					GL.Disable (EnableCap.DepthTest);
+				return result;
+			};
+		}
+		
+		public static Reaction<T> Blending<T> (this Reaction<T> render, 
+			BlendingFactorSrc source = BlendingFactorSrc.Src1Alpha,
+			BlendingFactorDest destination = BlendingFactorDest.OneMinusSrc1Alpha)
+		{
+			return input =>
+			{
+				var oldCap = GL.IsEnabled (EnableCap.Blend);
+				var oldSource = GetState (GetPName.BlendSrc);
+				var oldDest = GetState (GetPName.BlendDst);
+
+				GL.Enable (EnableCap.Blend);
+				GL.BlendFunc (source, destination);
+				var result = render (input);
+				GL.BlendFunc ((BlendingFactorSrc)oldSource, (BlendingFactorDest)oldDest);
+				if (!oldCap)
+					GL.Disable (EnableCap.Blend);
+				return result;
+			};
+		}
+
+		public static Reaction<T> DrawBuffer<T> (this Reaction<T> render, DrawBufferMode mode)
+		{
+			return input =>
+			{
+				var oldMode = GetState (GetPName.DrawBuffer);
+				GL.DrawBuffer (mode);
+				var result = render (input);
+				GL.DrawBuffer ((DrawBufferMode)oldMode);
+				return result;
+			};
+		}
+
+		public static Reaction<T> Program<T> (this Reaction<T> render, Program program)
+		{
+			return input =>
+			{
+				using (program.Scope ())
+					return render (input);
+			};
+		}
+
+		public static Reaction<T> Texture<T> (this Reaction<T> render, Texture texture)
+		{
+			return input =>
+			{
+				using (texture.Scope ())
+					return render (input);
+			};
+		}
+
+		public static Reaction<T> Framebuffer<T> (this Reaction<T> render, Framebuffer framebuffer)
+		{
+			return input =>
+			{
+				using (framebuffer.Scope ())
+					return render (input);
+			};
+		}
+
+		public static Reaction<T> BindSamplers<T> (this Reaction<T> render, IDictionary<Sampler, Texture> bindings)
+		{
+			return input =>
+			{
+				Sampler.Bind (bindings);
+				var result = render (input);
+				Sampler.Unbind (bindings);
+				return result;
+			};
+		}
+	}
+}

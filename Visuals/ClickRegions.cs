4e85d5284841456c19e4bc35beb2694b2bfa7f2b
Tommi <tommi.johtela@dnainternet.net>
2016-05-12 17:01:07 +0300

    Intermediate commit.

---
+++ Visuals/ClickRegions.cs
@@ -0,0 +1,25 @@
+﻿namespace Visuals
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Drawing;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class ClickRegions
+	{
+		private List<Tuple<RectangleF, Action>> _regions = 
+			new List<Tuple<RectangleF, Action>> ();
+
+		public void Clear ()
+		{
+			_regions.Clear ();
+		}
+
+		Action<RectangleF> Add (Action action)
+		{
+			return rect => _regions.Add (Tuple.Create (rect, action));
+		}
+	}
+}

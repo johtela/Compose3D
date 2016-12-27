764d85ea5a127476cbfeb2a4c02574ba7928b5ea
Tommi Johtela <tommi.johtela@dnainternet.net>
2016-12-27 22:34:41 +0200

    Thinking continues.

--- Compose3D/Parallel/ParSignal.cs
+++ Compose3D/Parallel/ParSignal.cs
@@ -2,18 +2,18 @@
 {
 	using System;
 	using System.Linq.Expressions;
+	using Compiler;
 	using CLTypes;
 	using Maths;
 
 	public static class ParSignal
 	{
-		//public static Expression<Func<uint>> FloatToUintGrayscale (Expression<Func<float>> signal)
-		//{
-		//	return signal. .Select (x =>
-		//	{
-		//		var c = (uint)(x.Clamp (0f, 1f) * 255f);
-		//		return c << 24 | c << 16 | c << 8 | 255;
-		//	});
-		//}
+		public static Func<uint> FloatToUintGrayscale (Expression<Func<float>> signal)
+		{
+			return CLProgram.Function (() => FloatToUintGrayscale (signal),
+				() => (from x in signal.Compile () ().ToKernel ()
+					   let c = (uint)(x.Clamp (0f, 1f) * 255f)
+					   select c << 24 | c << 16 | c << 8 | 255).Evaluate ());
+		}
 	}
 }

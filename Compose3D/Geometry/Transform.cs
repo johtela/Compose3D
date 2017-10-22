ce9082d0d9c3810c6d92438c29bc95c5f04e3648
tommi <tommi@KULTAKUTRI>
2017-10-22 22:08:10 +0300

    Small optimization.

--- Compose3D/Geometry/Transform.cs
+++ Compose3D/Geometry/Transform.cs
@@ -7,21 +7,20 @@
 	internal class Transform<V> : Wrapper<V> where V : struct, IVertex
 	{
         internal Mat4 _matrix;
-        internal Mat3 _normalMatrix;
 
 		public Transform (Geometry<V> geometry, Mat4 matrix)
 			: base (geometry)
 		{
 			_matrix = matrix;
-			_normalMatrix = new Mat3(_matrix).Inverse.Transposed;
 		}
 
 		protected override IEnumerable<V> GenerateVertices ()
 		{
+            var normalMatrix = new Mat3 (_matrix).Inverse.Transposed;
             return _geometry.Vertices.Select (v =>
             {
                 v.position = new Vec3 (_matrix * new Vec4 (v.position, 1f));
-                v.normal = (_normalMatrix * v.normal).Normalized;
+                v.normal = (normalMatrix * v.normal).Normalized;
                 return v;
             });
         }
